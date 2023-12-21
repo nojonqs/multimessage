adb9b36aca8ab7dfc5804dd9128b34fe8a360a12
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-21 19:05:21 +0100

    Add basic Contact and ContactList models

--- multimessage/contacts/models.py
+++ multimessage/contacts/models.py
@@ -1,2 +1,18 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
+
+class Contact(models.Model):
+    display_name = models.CharField(max_length=64, null=False, blank=True)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+
+    def __str__(self):
+       return f"{self.display_name} ({self.phone_number})"
+
+
+class ContactList(models.Model):
+  display_name = models.CharField(max_length=64, null=False, blank=True)
+  contacts = models.ManyToManyField(Contact)
+
+  def __str__(self):
+     return f"{self.display_name}"
--- multimessage/multimessage/settings.py
+++ multimessage/multimessage/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 # Application definition
 
 INSTALLED_APPS = [
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,6 +120,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+PHONENUMBER_DEFAULT_REGION = 'DE'
 
 
 # Static files (CSS, JavaScript, Images)
