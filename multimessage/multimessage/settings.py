16f73117cd661cbc1634e158369df0b0b213eef9
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-23 00:28:25 +0100

    Add a base template with basic html stuff which every other template can extend

--- multimessage/multimessage/settings.py
+++ multimessage/multimessage/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'multimessage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'multimessage' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
