4520fe329cd01006700d4074c0bd60c7abd0b04b
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-25 02:38:41 +0100

    Add a signal setup try (that only works if the device was already paired in the past) on django startup

--- multimessage/contacts/apps.py
+++ multimessage/contacts/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from .signal_helper import setup_signal_bot_if_device_linked
 
 class ContactsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contacts'
+
+    def ready(self) -> None:
+        setup_signal_bot_if_device_linked()
+        
+        return super().ready()
