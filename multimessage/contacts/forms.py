e522500d3c201e011115de961e0357bb607bd95e
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-26 01:43:04 +0100

    Add a SendMessageForm to make handling the data for sending messages (sender, message, recipiants) easier between templates and django code
    Some imports do not work since I committed this during a WIP setting

--- multimessage/contacts/forms.py
+++ multimessage/contacts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Contact, ContactList
+from contacts.models import Contact, ContactList
+from contacts.signal_helper import get_linked_phone_numbers
 
 class ContactCreateForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,13 @@ class ContactListCreateForm(forms.ModelForm):
     class Meta:
         model = ContactList
         fields = ["display_name", "contacts"]
+
+def get_linked_phone_numbers_as_dict():
+    d = {n: n for n in get_linked_phone_numbers()}
+    return d
+
+class SendMessageForm(forms.Form):
+    sender = forms.ChoiceField(choices=get_linked_phone_numbers_as_dict)
+    message = forms.CharField(max_length=2048, widget=forms.Textarea)
+    single_recipiants = forms.ModelMultipleChoiceField(queryset=Contact.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
+    group_recipiants = forms.ModelMultipleChoiceField(queryset=ContactList.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
