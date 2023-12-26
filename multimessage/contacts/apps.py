04a3beb5ec4df867b562d480c1fcee7e0dfc48f0
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-26 01:38:28 +0100

    Move asyncio setup to apps.py so it only runs once at startup

--- multimessage/contacts/apps.py
+++ multimessage/contacts/apps.py
@@ -1,11 +1,15 @@
 from django.apps import AppConfig
-from .signal_helper import setup_signal_bot_if_device_linked
+import asyncio
 
 class ContactsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contacts'
 
     def ready(self) -> None:
+        from .signal_helper import setup_signal_bot_if_device_linked
         setup_signal_bot_if_device_linked()
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
         
         return super().ready()
