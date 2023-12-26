a1b386d39b1551611bb70fc183033b939e229028
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-26 17:17:36 +0100

    Make entrypoint.sh automatically create a superuser

--- multimessage/entrypoint.sh
+++ multimessage/entrypoint.sh
@@ -1,4 +1,6 @@
-python3 manage.py makemigrations
+set -x
+
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
+python3 manage.py createsuperuser --noinput
 python3 manage.py runserver 0.0.0.0:${APP_PORT}
