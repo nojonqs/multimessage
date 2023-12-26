c34263ad1a73d9e9e8c1f6799882e898e2f99379
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-26 01:44:16 +0100

    Add the relevant signal related django views to views.py

--- multimessage/contacts/views.py
+++ multimessage/contacts/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.views import generic
 from django.urls import reverse_lazy
+from django.shortcuts import render
 from .models import Contact, ContactList
 from .forms import ContactCreateForm, ContactListCreateForm
+from .signal_helper import is_signal_linked, is_signal_bot_setup, send_message_to
 
 
 def index(request):
@@ -27,3 +29,40 @@ class ContactListCreateView(generic.CreateView):
     model = ContactList
 
     success_url = reverse_lazy('contact:index')
+
+
+def prepare_message(request):
+    if request.method == 'POST':
+        from contacts.forms import SendMessageForm
+        f = SendMessageForm(request.POST)
+
+        if not f.is_valid():
+            raise ValueError(
+                "SEND_MESSAGE: post request does not fit the SendMessageForm")
+
+        message = f.cleaned_data['message']
+
+        for recipiant in f.cleaned_data['single_recipiants']:
+            print(f"SEND_MESSAGE: sending message to {recipiant}: '{message}'")
+            send_message_to(recipiant, message)
+
+        for group in f.cleaned_data['group_recipiants']:
+            group: ContactList
+            for recipiant in group.contacts.all():
+                send_message_to(recipiant, message)
+
+        return render(request, 'contacts/send_message.html', {'form': f})
+
+    if request.method == 'GET':
+        from contacts.forms import SendMessageForm
+        f = SendMessageForm()
+        return render(request, 'contacts/send_message.html', {'form': f})
+
+
+def setup(request):
+    global signal_bot
+    context = {
+        'is_signal_linked': is_signal_linked(),
+        'is_bot_setup': is_signal_bot_setup(),
+    }
+    return render(request, 'contacts/signal_setup.html', context)
