8abbb68ce9c845d52f425e50fd27227f747ad04d
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-12-29 02:59:05 +0100

    Use wait-for-it in entrypoint.sh to wait for the db to be ready

--- multimessage/entrypoint.sh
+++ multimessage/entrypoint.sh
@@ -1,6 +1,10 @@
 set -x
 
-sleep 5
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]
+then
+    ./wait-for-it/wait-for-it.sh "${DB_HOST}:${DB_PORT}" --timeout=300
+fi
+
 python3 manage.py showmigrations
 python3 manage.py migrate -v 3 --noinput
 python3 manage.py collectstatic --noinput
