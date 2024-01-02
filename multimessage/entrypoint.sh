ef89bad2f1d35432bb93b9bf97803c53cb0fbc2b
Jonas Bergmann <bergmann.jonas31@gmail.com>
2024-01-02 03:34:11 +0100

    Rename settings.py to settings_prod.py

--- multimessage/entrypoint.sh
+++ multimessage/entrypoint.sh
@@ -5,8 +5,9 @@ then
     ./wait-for-it/wait-for-it.sh "${DB_HOST}:${DB_PORT}" --timeout=300
 fi
 
-python3 manage.py showmigrations
-python3 manage.py migrate -v 3 --noinput
-python3 manage.py collectstatic --noinput
-python3 manage.py createsuperuser --noinput
-python3 manage.py runserver 0.0.0.0:${APP_PORT}
+python3 manage.py showmigrations --settings multimessage.settings_prod
+python3 manage.py makemigrations contacts --settings multimessage.settings_prod --noinput
+python3 manage.py migrate -v 3 --settings multimessage.settings_prod --noinput
+python3 manage.py collectstatic --settings multimessage.settings_prod --noinput
+python3 manage.py createsuperuser --settings multimessage.settings_prod --noinput
+python3 manage.py runserver 0.0.0.0:${APP_PORT} --settings multimessage.settings_prod
