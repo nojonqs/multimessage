2c15aa388d3fd5cc88f0203a5dac1d72b9915d64
Jonas Bergmann <bergmann.jonas31@gmail.com>
2024-01-04 02:45:27 +0100

    Add a uuid for the Contact model

--- multimessage/contacts/models.py
+++ multimessage/contacts/models.py
@@ -5,9 +5,10 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Contact(models.Model):
     display_name = models.CharField(max_length=64, null=False, blank=True)
     phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    uuid = models.CharField(max_length=64, null=False)
 
     def __str__(self):
-        return f"{self.display_name} ({self.phone_number})"
+        return f"{self.display_name}, {self.phone_number} ({self.uuid})"
 
 
 class Group(models.Model):
