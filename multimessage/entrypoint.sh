7dcebc57684a47138f4040647755ff516899dd9a
Jonas Bergmann <bergmann.jonas31@gmail.com>
2024-01-04 04:07:40 +0100

    Add logging via a file to the signal-cli daemon

--- multimessage/entrypoint.sh
+++ multimessage/entrypoint.sh
@@ -1,6 +1,6 @@
 set -x
 
-signal-cli -v --config $SIGNAL_CLI_CONFIG daemon --socket $SIGNAL_CLI_SOCKET &
+signal-cli -v --config $SIGNAL_CLI_CONFIG --log-file $SIGNAL_CLI_LOGS/$(date -d "today" +"%Y%m%d%H%M").log daemon --no-receive-stdout --socket $SIGNAL_CLI_SOCKET &
 
 if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]
 then
@@ -8,7 +8,6 @@ then
 fi
 
 python3 manage.py showmigrations --settings multimessage.settings_prod
-python3 manage.py makemigrations contacts --settings multimessage.settings_prod --noinput
 python3 manage.py migrate -v 3 --settings multimessage.settings_prod --noinput
 python3 manage.py collectstatic --settings multimessage.settings_prod --noinput
 python3 manage.py createsuperuser --settings multimessage.settings_prod --noinput
