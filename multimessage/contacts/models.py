316738c84c080238c2f3b4f0f83505f6559357db
Jonas Bergmann <bergmann.jonas31@gmail.com>
2024-01-05 22:17:51 +0100

    Add some functionality to Contact model and change a member of it I changed the uuid member of the Contact model to have its uuid member be allowed blank since we want to allow users to create new Contact models without knowing the uuid. We can fetch the uuid when saving the model ourselves

--- multimessage/contacts/models.py
+++ multimessage/contacts/models.py
@@ -5,10 +5,20 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Contact(models.Model):
     display_name = models.CharField(max_length=64, null=False, blank=True)
     phone_number = PhoneNumberField(null=False, blank=False, unique=True)
-    uuid = models.CharField(max_length=64, null=False)
+    uuid = models.CharField(max_length=64, null=False, blank=True)
+
+    @property
+    def international_number(self):
+        return self.phone_number.as_international
+
+    def save(self, *args, **kwargs):
+        if not self.uuid:
+            from contacts.signal_helper import get_uuid_for_number
+            self.uuid = get_uuid_for_number(self.international_number)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.display_name}, {self.phone_number} ({self.uuid})"
+        return f"{self.display_name}, {self.international_number} ({self.uuid})"
 
 
 class Group(models.Model):
